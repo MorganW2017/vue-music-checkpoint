e5edd3c09bc2b12480618969a22f3ff379175101
Morgan Waters <morangwaters@gmail.com>
2017-12-01 14:00:37 -0700

    christmas is coming

--- server/mlab/mlab-config.js
+++ server/mlab/mlab-config.js
@@ -2,9 +2,6 @@ var mongoose = require('mongoose')
 var connectionString = "mongodb://test:test@ds040877.mlab.com:40877/vue-tunes"
 var connection = mongoose.connection
 
-
-
-
 mongoose.connect(connectionString, {
     useMongoClient: true,
     keepAlive: {socketOptions: {keepAlive: 300000, connectionTimeoutMS: 30000}}
